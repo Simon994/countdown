77b9bde3b1ff1d03982082abb75f67bbabd04c30
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-28 10:43:04 +0100

    Add styling and setInterval for countdown

--- EventList.js
+++ EventList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
-import { Text, FlatList } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 
 import EventCard from './EventCard'
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 40,
+    backgroundColor: 'red'
+  }
+})
 
 class EventList extends Component {
   state = {
@@ -9,6 +17,15 @@ class EventList extends Component {
   }
   
   componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now(),
+        })),
+      })
+    }, 1000)
+    
     const events = require('./db.json').events.map(event => ({
       ...event,
       date: new Date(event.date)
@@ -18,12 +35,13 @@ class EventList extends Component {
 
   render() {
     return (
-      <FlatList 
+      <FlatList
         data={this.state.events}
         renderItem={
           ({ item }) => <EventCard event={ item }/>
         }
         keyExtractor={ item => item.id }
+        style={styles.list}
       />
     )
   }
