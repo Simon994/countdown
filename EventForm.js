e6c09bcfd65dc21f9948a8ebfdd93681abbea46c
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-29 10:26:42 +0100

    Update EventForm. Add DateTimePicker package

--- EventForm.js
+++ EventForm.js
@@ -1,24 +1,80 @@
 import React, { Component } from 'react'
 import {
   View,
+  StyleSheet,
   Text,
   TouchableHighlight,
+  TextInput
 } from 'react-native'
 
 
+const styles = StyleSheet.create({
+  fieldContainer: {
+    margin: 20,
+    backgroundColor: 'blue'
+  },
+  eventInput: {
+    backgroundColor: '#333',
+    color: 'white',
+    padding: 10,
+    fontSize: 22
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    alignSelf: 'stretch',
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+  }
+})
+
 class EventForm extends Component {
-  
+
+  state = {
+    text: ''
+  }
+
+
   handleAddPress = () => {
+    console.log(this.state)
     this.props.navigation.navigate('Home')
+  }
+
+  handleChangeText = (text) => {
+    this.setState({
+      text
+    })
   }
 
   render() {
     return (
-      <View>
+      <View
+        style={{
+          flex: 1
+        }}
+      >
+        <View style={styles.fieldContainer}>
+          <TextInput
+            placeholder='Event title'
+            style={styles.eventInput}
+            placeholderTextColor='white'
+            spellCheck={false}
+            onChangeText={this.handleChangeText}
+            value={this.state.text}
+          ></TextInput>
+        </View>
         <TouchableHighlight
           onPress={this.handleAddPress}
+          style={styles.button}
         >
-          <Text>Add</Text>
+          <Text style={styles.buttonText}>Add</Text>
         </TouchableHighlight>
       </View>
     )
